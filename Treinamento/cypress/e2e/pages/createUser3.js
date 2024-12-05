4f8b4f0254392833f8f6e6cd89a525c0fd1cd059
Jefersson Lima <jeferssonlima4@hotmail.com>
2024-12-05 14:48:36 -0300

    test 1

--- Treinamento/cypress/e2e/pages/createUser3.js
+++ Treinamento/cypress/e2e/pages/createUser3.js
@@ -1,32 +1,55 @@
-import { randFirstName, randLastName, randEmail, randPhoneNumber, randStreetAddress,
-         randCity, randZipCode, randUserName, randCompanyName } from '@ngneat/falso';
+import { 
+    randFirstName, randLastName, randEmail, randPhoneNumber, 
+    randStreetAddress, randCity, randZipCode, randUserName, 
+    randCompanyName 
+} from '@ngneat/falso';
 
-function navegation(){
-
-    cy.visit('https://automationteststore.com/index.php?rt=account/create');
-}
 class UserCasual {
-    createUserWithNgneat() {
-        navegation();
+    navigateToCreateAccountPage() {
+        cy.visit('https://automationteststore.com/index.php?rt=account/create');
+    }
 
-        cy.get('#AccountFrm_firstname').type(randFirstName());
-        cy.get('#AccountFrm_lastname').type(randLastName());
-        cy.get('#AccountFrm_email').type(randEmail());
-        cy.get('#AccountFrm_telephone').type(randPhoneNumber());
-        cy.get('#AccountFrm_fax').type(randPhoneNumber());
-        cy.get('#AccountFrm_company').type(randCompanyName());
-        cy.get('#AccountFrm_address_1').type(randStreetAddress());
-        cy.get('#AccountFrm_address_2').type(randStreetAddress());
-        cy.get('#AccountFrm_city').type(randCity());
+    fillField(selector, value) {
+        cy.get(selector).type(value);
+    }
+
+    createUserWithNgneat() {
+        this.navigateToCreateAccountPage();
+
+        const userData = {
+            firstName: randFirstName(),
+            lastName: randLastName(),
+            email: randEmail(),
+            phone: randPhoneNumber(),
+            company: randCompanyName(),
+            address1: randStreetAddress(),
+            address2: randStreetAddress(),
+            city: randCity(),
+            zipCode: randZipCode(),
+            username: randUserName(),
+            password: 'Snow@123',
+        };
+
+        this.fillField('#AccountFrm_firstname', userData.firstName);
+        this.fillField('#AccountFrm_lastname', userData.lastName);
+        this.fillField('#AccountFrm_email', userData.email);
+        this.fillField('#AccountFrm_telephone', userData.phone);
+        this.fillField('#AccountFrm_fax', userData.phone);
+        this.fillField('#AccountFrm_company', userData.company);
+        this.fillField('#AccountFrm_address_1', userData.address1);
+        this.fillField('#AccountFrm_address_2', userData.address2);
+        this.fillField('#AccountFrm_city', userData.city);
+
         cy.get('#AccountFrm_country_id').select('Brazil');
-        cy.wait(1000);
-        cy.get('#AccountFrm_zone_id').select('Pernambuco');
-        cy.get('#AccountFrm_postcode').type(randZipCode());
-        cy.get('#AccountFrm_loginname').type(randUserName());
-        cy.get('#AccountFrm_password').type('Snow@123');
-        cy.get('#AccountFrm_confirm').type('Snow@123');
-        cy.get('#AccountFrm_newsletter1').click();
-        cy.get('#AccountFrm_agree').click();
+        cy.get('#AccountFrm_zone_id').should('be.visible').select('Pernambuco');
+
+        this.fillField('#AccountFrm_postcode', userData.zipCode);
+        this.fillField('#AccountFrm_loginname', userData.username);
+        this.fillField('#AccountFrm_password', userData.password);
+        this.fillField('#AccountFrm_confirm', userData.password);
+
+        cy.get('#AccountFrm_newsletter1').check();
+        cy.get('#AccountFrm_agree').check();
 
         cy.get('.col-md-2 > .btn').click();
     }
